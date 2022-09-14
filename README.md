992fe4f12a12d0632c8a56ad5f116f70299fd9cf
Will D <will.dean0103@gmail.com>
2022-09-13 21:01:55 -0400

    finished readme

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## description
 
-[My-github]()
+[My-github](https://github.com/Will-Dean5/team-profile-generator)
 
 This is used to add employees to a team. Their is a manager and you have a choice to add engineers and interns as well. Each position has a name, id, and email. The manager has an office number. The engineer has a github username, and the intern has a school that was attended. Once you add as many employees as you want you can hit im done and a html page will generate with the employees on it.
 
--- src/page-template.js
+++ src/page-template.js
@@ -11,13 +11,13 @@ function generateHtml(team) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body class="text-light bg-dark vh-100">
-        <h1 class="text-center mt-3">My Team</h1>
+        <h1 class="text-center mx5-3">My Team</h1>
         <div class="container-fluid d-flex h-100 align-items-center justify-content-center">
         ${team.map(employee => {
-            return generateCard(employee)
-        })
-        .join('')
-    }
+        return generateCard(employee)
+    })
+            .join('')
+        }
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
@@ -26,7 +26,7 @@ function generateHtml(team) {
 }
 function generateCard(employee) {
     var template = `
-    <div class="card text-white bg-danger mx-3" style="max-width: 20rem;">
+    <div class="card text-white bg-danger mx-5" style="max-width: 18rem;">
         <div class="card-header">${employee.name}</div>
         <div class="card-body">
           <h4 class="card-title">${employee.id}</h4>
@@ -34,7 +34,7 @@ function generateCard(employee) {
           ${lastProperty(employee)}
         </div>
     </div>`
-return template;
+    return template;
 }
 function lastProperty(employee) {
     var finalProp = '';
@@ -42,7 +42,7 @@ function lastProperty(employee) {
         finalProp = employee.officeNum
     } else if (employee.getRole() === 'Intern') {
         finalProp = employee.School
-    } else if (employee.getRole() === 'Engineer'){
+    } else if (employee.getRole() === 'Engineer') {
         finalProp = employee.GitHub
     }
     var template = `<p class="card-text">${finalProp}</p> `
